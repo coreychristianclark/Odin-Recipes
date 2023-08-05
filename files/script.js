ff90b774fc568630aa2cdfcc600b76cbda18ea4f
Corey Clark <coreychristianclark@gmail.com>
2023-08-05 11:39:49 -0400

    The navbar successfully reloads with everything else

--- files/script.js
+++ files/script.js
@@ -1,11 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/files/html-files/navBar.html')
-      .then(response => response.text())
-      .then(html => {
-        const navbarPlaceholder = document.querySelector('#navbar-placeholder');
-        navbarPlaceholder.innerHTML = html;
-      })
-      .catch(error => {
-        console.error('Error fetching navbar content:', error);
-      })
-  })
+const URL = "/files/html-files/navBar.html";
+
+async function loadNavbarContent() {
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const html = await response.text();
+    const navbarPlaceholder = document.querySelector("#navbar-placeholder");
+    navbarPlaceholder.innerHTML = html;
+
+    window.scrollTo(0, 0);
+  } catch (error) {
+    console.error("Error fetching navbar content:", error);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+  loadNavbarContent();
+});
+
+window.addEventListener("beforeunload", () => {
+  window.scrollTo(0, 0);
+});
